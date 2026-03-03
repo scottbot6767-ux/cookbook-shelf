ddad01ef32117ee380baa2f32b02777712992538
scottbot6767-ux <scottbot6767@gmail.com>
2026-03-03 02:12:11 +0000

    Fix imports and remove env config

--- next.config.js
+++ next.config.js
@@ -9,10 +9,7 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
-  },
+
 };
 
 module.exports = nextConfig;
